ddadef5a5f6587b98f1b1800478b396af252dd3d
Gianmarco Mameli <gnammyx@gmail.com>
2024-10-11 23:08:19 +0200

    working

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ warnings.filterwarnings("ignore")
 import os
 import signal
 import socket
-import subprocess
+# import subprocess
 import sys
 import threading
 import time
@@ -14,231 +14,67 @@ import paho.mqtt.client as mqtt
 import psutil
 import requests
 from hurry.filesize import size
-from luma.core.interface.serial import spi
 import luma.core.render
-from luma.core.sprite_system import framerate_regulator
+from luma.core.interface.serial import spi
 from luma.lcd.device import st7789
-from PIL import Image, ImageDraw, ImageFont, ImageSequence
+from luma.core.sprite_system import framerate_regulator
+from PIL import Image, ImageDraw, ImageFont
 import random
-# from private import *
 
 graphite_url = os.getenv("GRAPHITE_URL")
 metric_group = os.getenv("METRIC_GROUP")
 mqtt_server = os.getenv("MQTT_SERVER")
 motion_topic = os.getenv("MOTION_TOPIC")
+lcd_rotation = int(os.getenv("LCD_ROTATION", 1))
 
-serial = spi(spi_mode=3, port=0, device=0, gpio_DC=17, gpio_RST=22)
-lcd = st7789(serial)
+serial = spi(spi_mode=3, port=0, device=0, gpio_DC=17, gpio_RST=22, bus_speed_hz=8000000)
+lcd = st7789(serial, rotate=lcd_rotation)
 width = lcd.width
 height = lcd.height
 
 default_color = "#FFFFFF"
-logo = Image.open("logo.png")
-# logo.draft('RGB',(60,47))
-
+logo = Image.open("raspberrypi.png")
 img = Image.new("RGB", (width, height), color=(0, 0, 0))
 draw = ImageDraw.Draw(img)
+imgss = Image.new("RGB", (width, height), color=(0, 0, 0))
+drawss = ImageDraw.Draw(imgss)
+
+# Boot splashscreen
 draw.rectangle((0, 0, width, height), outline=0, fill=0)
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20)
 font_big = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
 boot_text_1 = "Raspberry Pi Stats"
 boot_text_2 = "Starting..."
+print(boot_text_1)
+print(boot_text_2)
 w = draw.textlength(text=boot_text_1, font=font_big)
 draw.text((int((width-w)/2),2), text=boot_text_1, font=font_big, fill=default_color)
 w = draw.textlength(text=boot_text_2, font=font_big)
 draw.text((int((width-w)/2),30), text=boot_text_2, font=font_big, fill=default_color)
 w = int(logo.width)
 img.paste(logo, (int((width-w)/2),90))
-
 lcd.display(img)
 
 padding = -2
 top = padding
 bottom = height - padding
-
 global x
 x = 0
-
 data = {}
+show_stats = True
 
-class start_stats(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super(start_stats, self).__init__(*args, **kwargs)
-        self.__flag = threading.Event() # The flag used to suspend the thread
-        self.__flag.set() # set to True
-        self.__running = threading.Event() # ID used to stop the thread
-        self.__running.set() # set running to True
-
-    def run(self):
-        while self.__running.isSet():
-            self.__flag.wait()
-            # img = Image.new("RGB", (width, height), color=(0, 0, 0))
-            while self.__flag.wait():
-                print('draw stats')
-                img = Image.new("RGB", (width, height), color=(0, 0, 0))
-                # draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-                y = top
-
-                Hostname = str(hostname)
-                state_color = "#FFFFFF"
-                draw.text((x, y), Hostname, font=font_big, fill=state_color)
-                y += font.getbbox(Hostname)[3]
-
-                Ip = str(ip)
-                state_color = "#FFFFFF"
-                draw.text((x, y), Ip, font=font_big, fill=state_color)
-                y += font.getbbox(Ip)[3]
-
-                y += 8
-                draw.line([(x, y), (width, y)], "#FFFFFF", 2)
-                y += 4
-
-                Load = "Cpu Load: " + str(data["cpu_used"])
-                state_color = set_color(
-                    data["cpu_used"], load_info["load_warn"], load_info["load_crit"]
-                )
-                draw.text((x, y), Load, font=font, fill=state_color)
-                y += font.getbbox(Load)[3]
-
-                Clock = "Cpu Clock: " + str(data["cpu_clock"])
-                state_color = set_color(data["cpu_clock"], (cpu_freq_max * 0.80), cpu_freq_max)
-                draw.text((x, y), Clock, font=font, fill=state_color)
-                y += font.getbbox(Clock)[3]
-
-                Temp = "Temp: " + str(data["cpu_temp"]) + "°C"
-                state_color = set_color(
-                    data["cpu_temp"],
-                    temp_info["temperature_warn"],
-                    temp_info["temperature_crit"],
-                )
-                draw.text((x, y), Temp, font=font, fill=state_color)
-                y += font.getbbox(Temp)[3]
-
-                Mem = "Mem: " + str(size(data["mem_used"])) + "/" + str(size(data["mem_total"]))
-                state_color = set_color(
-                    data["mem_used"], mem_info["mem_warn"], mem_info["mem_crit"]
-                )
-                draw.text((x, y), Mem, font=font, fill=state_color)
-                y += font.getbbox(Mem)[3]
-
-                Disk = "Disk: " + str(size(data["df"])) + "/" + str(size(data["df_total"]))
-                state_color = set_color(
-                    data["df"], disk_info["disk_warn"], disk_info["disk_crit"]
-                )
-                draw.text((x, y), Disk, font=font, fill=state_color)
-                y += font.getbbox(Disk)[3]
-
-                Net = "Net: " + str(size(data["net_data"])) + "/" + str(net_speed) + "M"
-                state_color = set_color(data["net_data"] / 1000, (net_speed * 0.80), net_speed)
-                draw.text((x, y), Net, font=font, fill=state_color)
-                y += font.getbbox(Net)[3]
-
-                Procs = "Procs: " + str(data["procs"])
-                state_color = set_color(
-                    data["procs"], procs_info["procs_warn"], procs_info["procs_crit"]
-                )
-                draw.text((x, y), Procs, font=font, fill=state_color)
-                y += font.getbbox(Procs)[3]
-
-                y += 8
-                draw.line([(x, y), (width, y)], "#FFFFFF", 2)
-                y += 4
-
-                cmd = "dmesg --level=err,warn | tail -1"
-                Dmesg = subprocess.check_output(cmd, shell=True).decode("utf-8")
-                size_x = font.getbbox(Dmesg)[2]
-                text_x = lcd.width
-                x2 = (time.time() - t_start) * 100
-                x2 %= size_x + width
-                draw.text((int(text_x - x2), y), Dmesg, font=font, fill=(255, 255, 255))
-
-                lcd.display(img)
-
-    def pause(self):
-        self.__flag.clear() # Set to False to block the thread
-
-    def resume(self):
-        self.__flag.set() # Set to True to stop the thread from blocking
-
-    def stop(self):
-        self.__flag.set() # Resume the thread from the suspended state, if it has been suspended
-        self.__running.clear() # set to False
-
-class start_screensaver(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super(start_screensaver, self).__init__(*args, **kwargs)
-        self.__flag = threading.Event() # The flag used to suspend the thread
-        self.__flag.set() # set to True
-        self.__running = threading.Event() # ID used to stop the thread
-        self.__running.set() # set running to True
-
-    def run(self):
-        while self.__running.isSet():
-            # print(self.__flag.wait())
-            self.__flag.wait() # Returns immediately when True, blocks when False until the internal flag is True and returns
-            print('draw screensaver')
-            # print(self.__flag.wait())
-            # screensaver_run()
-            # img = Image.new("RGB", (width, height), color=(0, 0, 0))
-            b = screensaver(width, height, 15 * 1.5, "blue")
-
-            frame_count = 0
-            fps = ""
-            # canvas = luma.core.render.canvas(lcd)
-
-            # regulator = framerate_regulator(fps=0)
-
-            # while num_iterations > 0:
-            # print(wait)
-            while self.__flag.wait():
-                # with Image.open("giphy.gif") as im:
-                    
-                #     index = 1
-                #     for frame in ImageSequence.Iterator(im):
-                #         # frame.save(f"frame{index}.png")
-                #         index += 1
-                        
-                #         # frame.draft('RGB',(50,20))
-                #         lcd.display(frame)
-                # with regulator:
-                    # num_iterations -= 1
-
-                    # frame_count += 1
-                    # with canvas as c:
-                        # c.rectangle(lcd.bounding_box, fill="black")
-                        # for b in balls:
-                b.update_pos()
-                b.draw()
-                lcd.display(img)
-                        # c.text((2, 0), fps, fill="white")
-
-                    # if frame_count % 20 == 0:
-                        # fps = "FPS: {0:0.3f}".format(regulator.effective_FPS())
-
-
-
-    def pause(self):
-        self.__flag.clear() # Set to False to block the thread
-
-    def resume(self):
-        self.__flag.set() # Set to True to stop the thread from blocking
-
-    def stop(self):
-        self.__flag.set() # Resume the thread from the suspended state, if it has been suspended
-        self.__running.clear() # set to False
 
 class screensaver(object):
-    def __init__(self, w, h, radius, color):
+    def __init__(self, w, h, image):
         self._w = w
         self._h = h
-        self._imgWidth = logo.width
-        self._imgHeight = logo.height
-        self._color = color
-        self._x_speed = 2 #(random.random() - 0.5) * 10
-        self._y_speed = 2 #(random.random() - 0.5) * 10
-        self._x_pos = random.random() / 2.0  # self._w # / 2.0
-        self._y_pos = random.random() / 2.0  # self._h # / 2.0
+        self._image = image
+        self._imgWidth = image.width
+        self._imgHeight = image.height
+        self._x_speed = 1 #(random.random() - 0.5) * 10
+        self._y_speed = 1 #(random.random() - 0.5) * 10
+        self._x_pos = random.random() * self._w / 2.0
+        self._y_pos = random.random() * self._h / 2.0
 
     def update_pos(self):
         if self._x_pos + self._imgWidth > self._w:
@@ -255,81 +91,38 @@ class screensaver(object):
         self._y_pos += self._y_speed
 
     def draw(self):
-        # canvas.ellipse((self._x_pos - self._radius, self._y_pos - self._radius,
-                    #    self._x_pos + self._radius, self._y_pos + self._radius), fill=self._color)
-        # canvas(logo)
-        # draw.rectangle(logo.getbbox(), outline=0, fill=0)
-        # draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        img = Image.new("RGB", (width, height), color=(0, 0, 0))
-        # img.paste(logo., (int(self._x_pos),int(self._y_pos)))
-        img.paste(logo, (int(self._x_pos),int(self._y_pos)))
-        # draw = ImageDraw.Draw(img)
-        # lcd.display(img)
+        imgss.paste(self._image, (int(self._x_pos),int(self._y_pos)))
+        # drawss.bitmap((int(self._x_pos),int(self._y_pos)), logo, )
+        
 
-# def screensaver_run():
-#     # num_iterations=sys.maxsize
-#     # colors = ["red", "orange", "yellow", "green", "blue", "magenta"]
-#     # b = [Ball(width, height, 15 * 1.5, colors[i % 6]) for i in range(1)]
-#     b = screensaver(width, height, 15 * 1.5, "blue")
+class SignalHandler:
+    shutdown_requested = False
 
-#     frame_count = 0
-#     fps = ""
-#     # canvas = luma.core.render.canvas(lcd)
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.request_shutdown)
+        signal.signal(signal.SIGTERM, self.request_shutdown)
 
-#     # regulator = framerate_regulator(fps=0)
+    def request_shutdown(self, *args):
+        print('Request to shutdown received, stopping')
+        lcd.cleanup()
+        self.shutdown_requested = True
 
-#     # while num_iterations > 0:
-#     # print(wait)
-#     while True:
-#         # with Image.open("giphy.gif") as im:
-            
-#         #     index = 1
-#         #     for frame in ImageSequence.Iterator(im):
-#         #         # frame.save(f"frame{index}.png")
-#         #         index += 1
-                
-#         #         # frame.draft('RGB',(50,20))
-#         #         lcd.display(frame)
-#         # with regulator:
-#             # num_iterations -= 1
-
-#             # frame_count += 1
-#             # with canvas as c:
-#                 # c.rectangle(lcd.bounding_box, fill="black")
-#                 # for b in balls:
-#         b.update_pos()
-#         b.draw()
-#         lcd.display(img)
-#                 # c.text((2, 0), fps, fill="white")
-
-#             # if frame_count % 20 == 0:
-#                 # fps = "FPS: {0:0.3f}".format(regulator.effective_FPS())
-
+    def can_run(self):
+        return not self.shutdown_requested
 
 def on_connect(client, userdata, flags, rc):
-    # print("Connected with result code "+str(rc))
+    print("MQTT Connected with result code "+str(rc))
     client.subscribe(motion_topic)
 
-def on_message(client, userdata, msg):
+def on_message(client, userdata, msg,):
+    global show_stats
     if "motion" in msg.topic:
         if str(msg.payload.decode("utf-8")) == "1":
             client.publish(hostname + "/stats_display", "1")
-            print(msg.topic)
-            thread_screensaver.pause()
-            thread_stats.resume()
+            show_stats = True
         if str(msg.payload.decode("utf-8")) == "0":
-            # lcd.backlight(False)
-            # img = Image.new("RGB", (width, height), color=(0, 0, 0))
-            # draw = ImageDraw.Draw(img)
-            thread_stats.pause()
-            thread_screensaver.resume()
-            
-            print(msg.payload.decode("utf-8"))
-            # thread_screensaver.run()
+            show_stats = False
             client.publish(hostname + "/stats_display", "1")
-
-def signal_term_handler(signal, frame):
-    sys.exit(0)
 
 def set_color(value, warn, crit):
     if warn != None and crit != None:
@@ -347,7 +140,6 @@ def first(iterable, default=None):
         for item in iterable:
             return item
     return default
-
 
 def intersect(a, b):
     return list(set(a) & set(b))
@@ -417,9 +209,9 @@ def get_data():
     procs_info = get_info("procs.procs", "perfdata.procs")
 
     if graphite_url != None:
-        print("Using Graphite for thresholds")
+        print("Using Graphite data")
 
-    print("Data fetched")
+    print("New data fetched")
 
     time.sleep(3600)
 
@@ -469,12 +261,10 @@ def update_data():
 
         time.sleep(2)
 
-# def stats_display():
-    
+def main(num_iterations=sys.maxsize):
 
-def main():
-
-    signal.signal(signal.SIGTERM, signal_term_handler)
+    # signal.signal(signal.SIGTERM, signal_term_handler)
+    signal_handler = SignalHandler()
     x = 0
 
     thread_get = threading.Thread(target=get_data, daemon=True)
@@ -482,6 +272,11 @@ def main():
 
     thread_update = threading.Thread(target=update_data)
     thread_update.start()
+
+    frame_count = 0
+    # fps = ""
+    # canvas = luma.core.render.canvas(lcd, dither=True)
+    regulator = framerate_regulator(fps=0)
 
     time.sleep(10)
 
@@ -491,24 +286,112 @@ def main():
         client.on_message = on_message
         client.connect(mqtt_server)
         client.loop_start()
-        print("Mqtt server connected")
 
     global t_start
     t_start = time.time()
 
-    global thread_screensaver
-    thread_screensaver = start_screensaver()
-    thread_screensaver.start()
+    # ss = screensaver(width, height)
+    ss = [screensaver(width, height, i) for i in [logo]]
 
-    global thread_stats
-    thread_stats = start_stats()
-    thread_stats.start()
+    while signal_handler.can_run():
+        while show_stats:
 
-    while True:
-        
-        # time.sleep(0.1)
-        pass
+            # img = Image.new("RGB", (width, height), color=(0, 0, 0))
+            # draw = ImageDraw.Draw(img)
+            draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
+            y = top
+
+            Hostname = str(hostname)
+            state_color = default_color
+            draw.text((x, y), Hostname, font=font_big, fill=state_color)
+            y += font.getbbox(Hostname)[3]
+
+            Ip = str(ip)
+            state_color = default_color
+            draw.text((x, y), Ip, font=font_big, fill=state_color)
+            y += font.getbbox(Ip)[3]
+
+            y += 8
+            draw.line([(x, y), (width, y)], default_color, 2)
+            y += 4
+
+            Load = "Cpu Load: " + str(data["cpu_used"])
+            state_color = set_color(
+                data["cpu_used"], load_info["load_warn"], load_info["load_crit"]
+            )
+            draw.text((x, y), Load, font=font, fill=state_color)
+            y += font.getbbox(Load)[3]
+
+            Clock = "Cpu Clock: " + str(data["cpu_clock"])
+            state_color = set_color(data["cpu_clock"], (cpu_freq_max * 0.80), cpu_freq_max)
+            draw.text((x, y), Clock, font=font, fill=state_color)
+            y += font.getbbox(Clock)[3]
+
+            Temp = "Temp: " + str(data["cpu_temp"]) + "°C"
+            state_color = set_color(
+                data["cpu_temp"],
+                temp_info["temperature_warn"],
+                temp_info["temperature_crit"],
+            )
+            draw.text((x, y), Temp, font=font, fill=state_color)
+            y += font.getbbox(Temp)[3]
+
+            Mem = "Mem: " + str(size(data["mem_used"])) + "/" + str(size(data["mem_total"]))
+            state_color = set_color(
+                data["mem_used"], mem_info["mem_warn"], mem_info["mem_crit"]
+            )
+            draw.text((x, y), Mem, font=font, fill=state_color)
+            y += font.getbbox(Mem)[3]
+
+            Disk = "Disk: " + str(size(data["df"])) + "/" + str(size(data["df_total"]))
+            state_color = set_color(
+                data["df"], disk_info["disk_warn"], disk_info["disk_crit"]
+            )
+            draw.text((x, y), Disk, font=font, fill=state_color)
+            y += font.getbbox(Disk)[3]
+
+            Net = "Net: " + str(size(data["net_data"])) + "/" + str(net_speed) + "M"
+            state_color = set_color(data["net_data"] / 1000, (net_speed * 0.80), net_speed)
+            draw.text((x, y), Net, font=font, fill=state_color)
+            y += font.getbbox(Net)[3]
+
+            Procs = "Procs: " + str(data["procs"])
+            state_color = set_color(
+                data["procs"], procs_info["procs_warn"], procs_info["procs_crit"]
+            )
+            draw.text((x, y), Procs, font=font, fill=state_color)
+            y += font.getbbox(Procs)[3]
+
+            y += 8
+            draw.line([(x, y), (width, y)], default_color, 2)
+            y += 4
+
+            # cmd = "dmesg --level=err,warn | tail -1"
+            # Dmesg = subprocess.check_output(cmd, shell=True).decode("utf-8")
+            # size_x = font.getbbox(Dmesg)[2]
+            # text_x = lcd.width
+            # x2 = (time.time() - t_start) * 100
+            # x2 %= size_x + width
+            # draw.text((int(text_x - x2), y), Dmesg, font=font, fill=(255, 255, 255))
+            
+            lcd.display(img)
+            time.sleep(0.5)
+
+        while not show_stats:
+            while num_iterations > 0:
+                with regulator:
+                    num_iterations -= 1
+
+                    frame_count += 1
+                    # with canvas as c:
+                    drawss.rectangle((0, 0, width, height), outline=0, fill=0)
+                    for s in ss:
+                        s.update_pos()
+                        s.draw()
+                    lcd.display(imgss)
+                    # if frame_count % 20 == 0:
+                    #     fps = "FPS: {0:0.3f}".format(regulator.effective_FPS())
 
 if __name__ == "__main__":
     try:
